d004a3a9dc72f1f524848a21079b753a08732dbd
ken-cherry <ken@cherrywebdev.com>
2023-12-20 03:32:03 -0600

    adjusted projects css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,34 +5,36 @@ const Projects = () => {
   return (
     <>
       <div className="content-divider"></div>
-      <section className="projects" id="projects">
+      <section className="projects section" id="projects">
         <div className="projects-title">
           <h2>projects</h2>
           <div className="underline"></div>
         </div>
-        <div className="project-center">
-          {/* <!-- projects small screen go here --> */}
-        </div>
-        <div className="projects-container">
+        {/* <div className="project-center">
+          
+        </div> */}
+        <div className="projects-container section-center">
           {projects.map((item) => {
             const { id, img, imgAlt, title, logo, text, link, logoAlt } = item;
             return (
-              <div className="card" key={id}>
-                <div className="card-side card-front">
-                  <img src={img} alt={imgAlt} className="img-link" />
-                  <h4>{title} </h4>
-                  <div className="card-icon">
-                    {/* <i className={logo}></i> */}
-                    <img src={logo} className="icon" alt={logoAlt} />
+              <article>
+                <div className="card" key={id}>
+                  <div className="card-side card-front">
+                    <img src={img} alt={imgAlt} className="img-link" />
+                    <h4>{title} </h4>
+                    <div className="card-icon">
+                      {/* <i className={logo}></i> */}
+                      <img src={logo} className="icon" alt={logoAlt} />
+                    </div>
+                  </div>
+                  <div className="card-side card-back">
+                    <p>{text} </p>
+                    <a href={link} target="_blank" rel="noreferrer">
+                      <button className="btn btn-card">visit site</button>
+                    </a>
                   </div>
                 </div>
-                <div className="card-side card-back">
-                  <p>{text} </p>
-                  <a href={link} target="_blank" rel="noreferrer">
-                    <button className="btn btn-card">visit site</button>
-                  </a>
-                </div>
-              </div>
+              </article>
             );
           })}
         </div>
